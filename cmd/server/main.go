e25d44a88a6a9a5c499890a8ed30d0e0b67c134e
Zallu35 <s14.sboss@gmail.com>
2026-07-04 13:49:35 -0400

    added to server main file and Dockerfile

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connString := "amqp://guest:guest@localhost:5672/"
+	rmqConnection, err := amqp.Dial(connString)
+	if err != nil {
+		fmt.Printf("Error connecting to RabbitMQ server: %v\n", err)
+		return
+	}
+	defer rmqConnection.Close()
+	fmt.Printf("Established connection to RabbitMQ server successfully\n")
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	<-sigChan
+	fmt.Printf("Closing server")
 }
